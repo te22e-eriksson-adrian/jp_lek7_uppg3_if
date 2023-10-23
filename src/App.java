c23458cbc556918c70c602fffc80dbd7c1421789
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2023-10-23 08:53:09 +0200

    uppg del a klar

--- src/App.java
+++ src/App.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner tangentbord = new Scanner(System.in);
+
+        //A
+        System.out.println("Detta är ett test för att se om du är berättigad för skatteavdrag för två bostäder.");
+        System.out.print("Hur långt ifrån din arbetsplats bor du (är du skriven): ");
+        int distans = tangentbord.nextInt();
+        tangentbord.nextLine();
+        if (distans>=50){
+            System.out.print("Är du ensamstående eller bor du tillsammans med någon (Svara 1 för ensam, 2 för icke ensam): ");
+            int bostatus = tangentbord.nextInt();
+            tangentbord.nextLine();
+            System.out.println("     ");
+            switch(bostatus)
+            {
+                case 1:
+                System.out.println("Du är berättigad skatteavdrag efter att du levt ensam i 1 år i två bostäder med avståndet 50 km eller mer.");
+                break;
+                case 2:
+                System.out.println("Du är berättigad skatteavdrag 3 år efter att du levt med din livskamrat i två bostäder med avståndet 50 km eller mer.");
+                break;
+                default:
+                System.out.println("Fel svar!");
+                break;
+            }
+        }else{
+            System.out.println("Du är tyvärr inte berättigad skatteavdrag.");
+        }
+        tangentbord.close();
     }
 }
